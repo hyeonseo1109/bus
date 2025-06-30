1384e1291d1a924a894a6ce9668b55b8f46a03fb
hyeonseo1109 <alsgustj2@naver.com>
2025-07-01 01:06:38 +0900

    ✨ 버스정류장 리스트 추가
    open api 이용하여 버스정류장 리스트를 추가하였음.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function BusStation() {
+  const [stations, setStations] = useState( [] );
+
+  useEffect( () => {
+    const getData = async () => {
+      try {
+      const data = await fetch('https://apis.data.go.kr/6410000/busrouteservice/v2/getBusRouteStationListv2?serviceKey=GS5aLVDaFTPnk4vYpexWNPYAl22LeqGp3N5duJZ3OqXXCfMOz0%2FleRDyv%2B6xHXvIogUG8jV2gCGaG04Hs39uRQ%3D%3D&routeId=241314001&format=json');
+      const json = await data.json();
+      setStations(json.response.msgBody.busRouteStationList);
+      } catch (err) {
+        console.error('에러 발생:', err);
+      }
+    };
+    getData();
+    }, [] );
 
   return (
     <>
-      <div>
-        <h1>안녕하세요</h1>
-        <h2>안녕</h2>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>버스정류장</h1>
+      <ul>
+        {stations.map( (station) => (
+          <div className="busStop">
+            <p>{station.stationName} (id: {station.stationId})</p>
+            <p>|</p>
+          </div>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default BusStation;
