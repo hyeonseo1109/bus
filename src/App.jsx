0703349ac1a1fe120db2d2d8e4515a3d47747da9
hyeonseo1109 <alsgustj2@naver.com>
2025-06-30 19:17:49 +0900

    h2 태그 추가

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <h1>안녕하세요</h1>
+        <h2>안녕</h2>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
