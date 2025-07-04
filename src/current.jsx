9509aaa20a679a307d4a9527a9146600ae5cf5d4
hyeonseo1109 <alsgustj2@naver.com>
2025-07-04 17:47:37 +0900

    🎨 코드 개선
    중첩되는 3항연산자 -> if문으로 변

--- src/current.jsx
+++ src/current.jsx
@@ -14,8 +14,17 @@ function BusLocation () {
                 // 실시간 버스가 여러 대 있을 때는 busLocationList가 배열로 오지만, 
                 // 딱 1대만 있을 때는 단일 객체 형태로 오고, 아예 없으면 null로 올 수 있음. 
                 console.log('real-time locations:', busLocations);
-                setLocation(busLocations ? (Array.isArray(busLocations) ? busLocations : [busLocations]) : []);
+                //setLocation(busLocations ? (Array.isArray(busLocations) ? busLocations : [busLocations]) : []);
                 // busLocations가 존재하면? 근데 그게 배열이 맞으면? 그냥 그대로 busLocations 쓰고 / 아니라면 빈 배열 추가하거나 빈 배열에 집어넣음.
+                if (busLocations) {
+                    if (Array.isArray(busLocations)) {
+                        setLocation(busLocations);
+                    } else {
+                        setLocation([busLocations]);
+                    } 
+                } else {
+                    setLocation( [] );
+                }
             } catch (err) {
                 console.error('에러 발생:', err);
             }
@@ -27,6 +36,7 @@ function BusLocation () {
         }, 10000);
     
         return () => clearInterval(interval);
+            //이거 없으면 계속 쌓여서 여러 개가 중첩됨
     }, []);
 
     return (
